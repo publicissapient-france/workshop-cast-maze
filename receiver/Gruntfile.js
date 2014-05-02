782de5fa6edcaafcc2bf9d04c704d1a5bcfcbc38
blacroix <benjlacroix@gmail.com>
2014-05-02 15:16:06 +0200

    Add grunt file to deploy automatically

---
+++ receiver/Gruntfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        copy: {
+            main: {
+                src: ['cast.js', 'game.js', 'index.html', 'jquery-1.11.0.min.js'], dest: 'dist/receiver/', filter: 'isFile'
+            }
+        },
+        'gh-pages': {
+            options: {
+                base: 'dist'
+            },
+            src: ['**']
+        },
+        'clean': ['dist']
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('dist', ['clean', 'copy']);
+};
