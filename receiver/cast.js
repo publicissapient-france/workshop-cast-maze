a5623cb7d178dcfa03d9ea49b1ab8b9f7a109a04
blacroix <benjlacroix@gmail.com>
2014-05-05 18:37:01 +0200

    Updates

--- receiver/cast.js
+++ receiver/cast.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     cast.receiver.logger.setLevelValue(0);
 
     window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
+
     log('Starting Receiver Manager');
 
     // handler for the 'ready' event
@@ -10,15 +11,12 @@ $(document).ready(function () {
         window.castReceiverManager.setApplicationState("Application status is ready...");
     };
 
-    // handler for 'senderconnected' event
     castReceiverManager.onSenderConnected = function (event) {
         log('Received Sender Connected event: ' + event.data);
         log(window.castReceiverManager.getSender(event.data).userAgent);
-        var color = addPlayer(event.senderId);
-        window.messageBus.send(event.senderId, {color: '#CCCCCC'});
+        addPlayer(event.senderId);
     };
 
-    // handler for 'senderdisconnected' event
     castReceiverManager.onSenderDisconnected = function (event) {
         log('Received Sender Disconnected event: ' + event.data);
         if (window.castReceiverManager.getSenders().length == 0) {
@@ -27,7 +25,6 @@ $(document).ready(function () {
         removePlayer(event.senderId);
     };
 
-    // handler for 'systemvolumechanged' event
     castReceiverManager.onSystemVolumeChanged = function (event) {
         log('Received System Volume Changed event: ' + event.data['level'] + ' ' +
             event.data['muted']);
@@ -36,8 +33,7 @@ $(document).ready(function () {
     // create a CastMessageBus to handle messages for a custom namespace
     window.messageBus =
         window.castReceiverManager.getCastMessageBus(
-            'urn:x-cast:fr.xebia.workshop.cast.maze',
-            cast.receiver.CastMessageBus.MessageType.JSON);
+            'urn:x-cast:fr.xebia.workshop.cast.maze');
 
     // handler for the CastMessageBus message event
     window.messageBus.onMessage = function (event) {
@@ -51,6 +47,7 @@ $(document).ready(function () {
 
     // initialize the CastReceiverManager with an application status message
     window.castReceiverManager.start({statusText: "Application is starting"});
+
     log('Receiver Manager started');
 
     /**
