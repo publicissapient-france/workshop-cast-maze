22a616d1748678bff704d751a910127ac50ed7ff
blacroix <benjlacroix@gmail.com>
2014-05-06 01:18:53 +0200

    Updates

--- receiver/cast.js
+++ receiver/cast.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+
+    window.castReceiverManager = null;
+    window.messageBus = null;
+
     cast.receiver.logger.setLevelValue(0);
 
     window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
@@ -9,27 +13,24 @@ $(document).ready(function () {
         window.castReceiverManager.setApplicationState("Application status is ready...");
     };
 
-    castReceiverManager.onSenderConnected = function (event) {
-        log('Received sender connected event ' + event.data);
-        var color = addPlayer(event.senderId);
-        log('Sending hey');
-        window.messageBus.send(event.senderId, 'hey');
-        log('Hey sent');
-    };
-
-    castReceiverManager.onSenderDisconnected = function (event) {
-        log('Received sender disconnected event: ' + event.data);
-        if (window.castReceiverManager.getSenders().length == 0) {
-            window.close();
-        }
-        removePlayer(event.senderId);
-    };
-
     window.castReceiverManager.start({statusText: "Application is starting"});
     log('Receiver manager started');
 
-    window.messageBus = window.castReceiverManager.getCastMessageBus('urn:x-cast:fr.xebia.workshop.cast.maze');
-    log('Message bus started');
+    castReceiverManager.onSenderConnected = function (event) {
+        log('Received sender connected event ' + event.data);
+        addPlayer(event.senderId);
+    };
+
+    castReceiverManager.onSenderDisconnected = function (event) {
+        log('Received sender disconnected event ' + event.data);
+        removePlayer(event.senderId);
+        if (window.castReceiverManager.getSenders().length == 0) {
+            window.close();
+        }
+    };
+
+    window.messageBus = window.castReceiverManager
+        .getCastMessageBus('urn:x-cast:fr.xebia.workshop.cast.maze');
 
     window.messageBus.onMessage = function (event) {
         log('Message [' + event.senderId + '] ' + event.data);
