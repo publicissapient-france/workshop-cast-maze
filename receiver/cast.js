49d70d4f745adbc0153b31e26fd9294c0cb423a4
blacroix <benjlacroix@gmail.com>
2014-05-05 18:30:20 +0200

    Updates

--- receiver/cast.js
+++ receiver/cast.js
@@ -2,25 +2,25 @@ $(document).ready(function () {
     cast.receiver.logger.setLevelValue(0);
 
     window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
-    console.log('Starting Receiver Manager');
+    log('Starting Receiver Manager');
 
     // handler for the 'ready' event
     castReceiverManager.onReady = function (event) {
-        console.log('Received Ready event: ' + JSON.stringify(event.data));
+        log('Received Ready event: ' + JSON.stringify(event.data));
         window.castReceiverManager.setApplicationState("Application status is ready...");
     };
 
     // handler for 'senderconnected' event
     castReceiverManager.onSenderConnected = function (event) {
-        console.log('Received Sender Connected event: ' + event.data);
-        console.log(window.castReceiverManager.getSender(event.data).userAgent);
+        log('Received Sender Connected event: ' + event.data);
+        log(window.castReceiverManager.getSender(event.data).userAgent);
         var color = addPlayer(event.senderId);
-        window.messageBus.send(event.senderId, color);
+        window.messageBus.send(event.senderId, {color: '#CCCCCC'});
     };
 
     // handler for 'senderdisconnected' event
     castReceiverManager.onSenderDisconnected = function (event) {
-        console.log('Received Sender Disconnected event: ' + event.data);
+        log('Received Sender Disconnected event: ' + event.data);
         if (window.castReceiverManager.getSenders().length == 0) {
             window.close();
         }
@@ -29,18 +29,19 @@ $(document).ready(function () {
 
     // handler for 'systemvolumechanged' event
     castReceiverManager.onSystemVolumeChanged = function (event) {
-        console.log('Received System Volume Changed event: ' + event.data['level'] + ' ' +
+        log('Received System Volume Changed event: ' + event.data['level'] + ' ' +
             event.data['muted']);
     };
 
     // create a CastMessageBus to handle messages for a custom namespace
     window.messageBus =
         window.castReceiverManager.getCastMessageBus(
-            'urn:x-cast:fr.xebia.workshop.cast.maze');
+            'urn:x-cast:fr.xebia.workshop.cast.maze',
+            cast.receiver.CastMessageBus.MessageType.JSON);
 
     // handler for the CastMessageBus message event
     window.messageBus.onMessage = function (event) {
-        console.log('Message [' + event.senderId + ']: ' + event.data);
+        log('Message [' + event.senderId + ']: ' + event.data);
         // display the message from the sender
         handleKeypress(event.data, event.senderId);
         // inform all senders on the CastMessageBus of the incoming message event
@@ -50,5 +51,16 @@ $(document).ready(function () {
 
     // initialize the CastReceiverManager with an application status message
     window.castReceiverManager.start({statusText: "Application is starting"});
-    console.log('Receiver Manager started');
+    log('Receiver Manager started');
+
+    /**
+     * Misc method to log into console box in web view
+     * @param msg message to log
+     */
+    function log(msg) {
+        var debug = document.getElementById('debug');
+        var tmpHTML = debug.innerHTML;
+        debug.innerHTML = '';
+        debug.innerHTML = JSON.stringify(msg) + '<br/>' + tmpHTML;
+    }
 });
